fd112f614625e2ca57b6a6e6ff5f886dfb76f105
bimsaru <bimsaru@orelit.com>
2025-09-23 18:58:06 +0530

    dockerfile and added variables into connectionString

--- DataAccessLayer/DependencyInjection.cs
+++ DataAccessLayer/DependencyInjection.cs
@@ -12,9 +12,17 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
     {
+        string connectionStringTemplate = configuration.GetConnectionString("DefaultConnection")!;
+
+        string connectionString = connectionStringTemplate.Replace("$MYSQL_HOST", Environment.GetEnvironmentVariable("MYSQL_HOST"))
+                                                          .Replace("$MYSQL_PASSWORD", Environment.GetEnvironmentVariable("MYSQL_PASSWORD"))
+                                                          .Replace("$MYSQL_PORT", Environment.GetEnvironmentVariable("MYSQL_PORT"));
+                                                          
+
+
         services.AddDbContext<ApplicationDbContext>(options =>
         {
-            options.UseMySQL(configuration.GetConnectionString("DefaultConnection")!);
+            options.UseMySQL(connectionString);
         });
 
         services.AddScoped<IProductRepository, ProductRepository>();
